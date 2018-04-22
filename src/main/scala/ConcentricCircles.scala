dcc0067f8183354769051d7419af3d0a77c00145
Edelita Valdez <edaleeta@gmail.com>
2018-04-21 17:44:05 -0700

    Adds change in color and transparency to concentric circles.

--- src/main/scala/ConcentricCircles.scala
+++ src/main/scala/ConcentricCircles.scala
@@ -5,12 +5,15 @@ import doodle.jvm.Java2DFrame._
 import doodle.backend.StandardInterpreter._
 
 object ConcentricCircles {
-  def concentricCircles(count: Int, size: Int): Image = {
+  def concentricCircles(count: Int, size: Int, color: Color): Image = {
     count match {
       case 0 => Image.empty
       case n =>
-        Image.circle(size + n * 10).lineWidth(3).lineColor(Color.royalBlue) on
-        concentricCircles(n-1, size)
+        val thisSize = n * 10 + size
+        val thisSpin = (n * 15).degrees
+        val thisAlpha = (1 - (n * 0.02)).normalized
+        Image.circle(thisSize).lineWidth(3).lineColor(color.spin(thisSpin).alpha(thisAlpha)) on
+        concentricCircles(n-1, size, color)
     }
   }
 }
